f83c93ac67738363cce96bd76b21d428c090b7d1
Local <aotnec@gmail.com>
2024-05-30 17:57:56 +0300

    update

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 import pandas as pd
 from pcap_handler import *
+from scapy.utils import rdpcap
+from packet_filter import *
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        #TODO: лена и соня - здесь кодьте главное окно
+
+
     def open_pcap(self, filename):
         pcap2df = pcapHandler(file=filename, verbose=True)
         df = pcap2df.to_DF(head=True)
-        print(df)
-
-
+        scapy_capture = rdpcap(filename)
+        return df, scapy_capture
 
 if __name__ == "__main__":
     app = QApplication([])
---
+++ packet_filter.py
@@ -0,0 +1,13 @@
+from scapy.utils import rdpcap
+from pcap_handler import *
+#TODO? other filters?
+filters = {"protocol": None, "ip_src": None, "ip_dst": None,  "size": None, "port": None, "contains": None, }
+
+
+def open_pcap(filename):
+    pcap2df = pcapHandler(file=filename, verbose=True)
+    df = pcap2df.to_DF(head=True)
+    scapy_packets = rdpcap(filename)
+    return df, scapy_packets
+def filter(filters, df):
+    pass
