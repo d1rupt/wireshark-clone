950589f3565bf52140185b99cfb1afc26780dc67
Local <test@test>
2024-06-01 19:43:19 +0300

    added menu example

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class MainWindow(QMainWindow):
         #TODO: лена и соня - здесь кодьте главное окно
         '''do-QAction действие которое возможно в интерфс.
          addaction назнач действ через do.add'''
+
+        '''
         toolbar = QToolBar(self)
         totoo = QToolBar(self)
         self.addToolBar(toolbar)
@@ -23,6 +25,14 @@ class MainWindow(QMainWindow):
 
         toolbar.addAction(butt_action)
         totoo.addAction(butt_action_2)
+        '''
+        # это прикольно, но я думала так; с помощью QMenu - я написала это еще в моём рисуночке)
+        self.menu = QMenuBar()
+        self.setMenuBar(self.menu) #создаем сверху менюшку
+
+        self.vis = QMenu("Visualisation", self) #добавляем туды кнопочку
+        self.menu.addMenu(self.vis)
+        #аналогично добавить New Capture
 
     def contextMenuEvent(self,event):
         event.ignore()
