5bf0732f8b6fe6b04f12aad215f60e9a977616dd
Cоня <sonya25215@bk.ru>
2024-06-02 18:42:20 +0300

    #создаю переменные         enter_ip = QLineEdit()         enter_port = QLineEdit()         enter_time = QLineEdit()         enter_contains = QLineEdit()         enter_size = QLineEdit()
            choose_protocol = QComboBox()
            choose_packet_type = QComboBox().

--- main.py
+++ main.py
@@ -23,23 +23,34 @@ class MainWindow(QMainWindow):
         self.capt = QMenu("New capture", self)
         self.menu.addMenu(self.capt)
 
+        #создаю переменные
+        enter_ip = QLineEdit()
+        enter_port = QLineEdit()
+        enter_time = QLineEdit()
+        enter_contains = QLineEdit()
+        enter_size = QLineEdit()
+
+        choose_protocol = QComboBox()
+        choose_packet_type = QComboBox()
+
         # Добавляем виджеты в GridLayout
         self.layout.addWidget(QLabel("IP"), 1, 0)
-        self.layout.addWidget(QLineEdit(), 1, 1)
+        self.layout.addWidget(enter_ip, 1, 1)
         self.layout.addWidget(QLabel("Port"), 1, 2)
-        self.layout.addWidget(QLineEdit(), 1, 3)
+        self.layout.addWidget(enter_port, 1, 3)
         self.layout.addWidget(QLabel("Protocol"), 1, 4)
-        self.layout.addWidget(QComboBox(), 1, 5)
+        self.layout.addWidget(choose_protocol, 1, 5)
         self.layout.addWidget(QLabel("Time"), 1, 6)
-        self.layout.addWidget(QLineEdit(), 1, 7)
+        self.layout.addWidget(enter_time, 1, 7)
         self.layout.addWidget(QLabel("Contains"), 2, 0)
-        self.layout.addWidget(QLineEdit(), 2, 1)
+        self.layout.addWidget(enter_contains, 2, 1)
         self.layout.addWidget(QLabel("Size"), 2, 2)
-        self.layout.addWidget(QLineEdit(), 2, 3)
+        self.layout.addWidget(enter_size, 2, 3)
         self.layout.addWidget(QLabel("Packet type"), 2, 4)
-        self.layout.addWidget(QComboBox(), 2, 5)
+        self.layout.addWidget(choose_packet_type, 2, 5)
         self.layout.addWidget(QPushButton("Apply"), 2, 6)
-        self.layout.addWidget(QTableWidget(self.main_widget), 4, 1)
+        self.table = QTableWidget(self.main_widget)
+        self.layout.addWidget(self.table, 3, 0, 8, 8)
 
         # Устанавливаем созданный виджет в качестве верхнего виджета
         self.setMenuWidget(self.main_widget)
