c9376920129dc8a6372ccb8c6dac8344433f24dc
Di <aotnec@gmail.com>
2024-06-14 13:53:48 +0300

    Revert "Revert "Merge remote-tracking branch 'origin/main'""
    This reverts commit cc3c6949

--- packet_filter.py
+++ packet_filter.py
@@ -1,14 +1,8 @@
 from scapy.utils import rdpcap
 from pcap_handler import *
 #TODO? other filters?
-filters = {"protocol": None, "ip_src": None, "ip_dst": None,  "size": None, "port": None, "contains": None, }
+filters = {"ip_src": None, "ip_dest":None, "port_src": None, "port_dest":None, "protocol": None, "contains": None, "size": None,}
 
-
-def open_pcap(filename):
-    pcap2df = pcapHandler(file=filename, verbose=True)
-    df = pcap2df.to_DF(head=True)
-    scapy_packets = rdpcap(filename)
-    return df, scapy_packets
 def filter(filters, df):
     #return COPY
     pass
